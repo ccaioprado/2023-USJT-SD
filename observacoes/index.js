1999e4f4f81768f617a9e951809a6cc0d9af6ec9
Caio Ryan <ccaio4567@gmail.com>
2023-03-24 21:51:55 -0300

    inicio da secao 4.3.8

---
+++ observacoes/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {v4: uuidv4} = require ('uuid')
+
+const app = express()
+app.use(bodyParser.json())
+
+const observacoesPorLembreteId = {}
+
+app.put('/lembretes/:id/observacoes',(req,res)=>{
+    const idObs = uuidv4()
+    const {texto} = req.body
+    const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || []
+    observacoesDoLembrete.push({id: idObs,texto})
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete
+    res.status(201).send(observacoesDoLembrete)
+})
+
+app.get('/lembretes/:id/observacoes',(req,res)=>{
+
+})
+
+app.listen(5000,() =>{
+    console.log("Serviço de observações, porta 5000")
+})
